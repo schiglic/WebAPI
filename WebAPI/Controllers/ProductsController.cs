3096de725cf8197a458f5590bd82c4a99c1df9f3
schiglic <1008stas2007@gmail.com>
2025-01-13 16:50:08 +0200

    Add project files.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        // Список продуктів з ID та назвами
+        private readonly Dictionary<int, string> products = new Dictionary<int, string>
+        {
+            { 1, "Phone" },
+            { 2, "Apples" },
+            { 3, "Interior Doors" }
+        };
+
+        // Метод для отримання всіх продуктів
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAllProducts()
+        {
+            return Ok(products.Values);
+        }
+
+        // Метод для отримання продукту за ID
+        [HttpGet("{id}")]
+        public ActionResult<string> GetProductById(int id)
+        {
+            if (products.TryGetValue(id, out var product))
+            {
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound($"Product with ID {id} not found");
+            }
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+
+app.UseDefaultFiles(); 
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
